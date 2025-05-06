fd9da811918d5a029bddebcef2e5d1b23623211a
ForzaElettromotrice <mingardi.federico@gmail.com>
2025-05-06 12:32:53 +0200

    Aggiornato lo script per creare il json degli spell. (Alcune descrizioni degli incantesimi sono troncate a metà però)

--- src/makeJSON.py
+++ src/makeJSON.py
@@ -12,7 +12,6 @@ def makeSpell(path:str)-> dict[str:Any]:
     soup = BeautifulSoup(html, 'html.parser')
 
     paragraph = soup.find("p").text
-    print(paragraph)
 
     spell = {
         "Nome": os.path.basename(path).replace(".html",    "")
@@ -26,10 +25,11 @@ def makeSpell(path:str)-> dict[str:Any]:
             spell["Livello"] = [tuple(x.strip().split(" ")) for x in line.split(":")[1].split(",")]
         for _type in ["Scuola", "Dominio", "Stirpe", "Componenti", "Tempo di Lancio", "Raggio di Azione", "Effetto", "Area", "Bersaglio", "Durata", "Tiro Salvezza", "Resistenza agli Incantesimi", "Descrizione"]:
             if line.startswith(_type):
-                spell[_type] = line.split(":")[1].strip()
+                try:
+                    spell[_type] = line.split(":")[1].strip()
+                except IndexError:
+                    spell[_type] = line.split(_type)[1].strip()
                 break
-    print()
-    print(spell)
     return spell
 
 
@@ -40,10 +40,7 @@ if __name__ == '__main__':
     for file in os.listdir("../data/html"):
         if file == "table.html":
             continue
-        if file != "Invisibilità.html":
-            continue
-        spells[file] = makeSpell(f"../data/html/{file}")
-        break
+        spells[file.replace(".html", "")] = makeSpell(f"../data/html/{file}")
 
     with open("../data/spells.json", "w", encoding = "utf-8") as f:
         json.dump(spells, f, indent = 8)
