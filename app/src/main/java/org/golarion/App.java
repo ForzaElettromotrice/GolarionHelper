d2ea8cfdd5712a5208188a2998de4deb6afb60ea
ForzaElettromotrice <mingardi.federico@gmail.com>
2025-08-01 17:05:42 +0200

    javafx initial commit

--- app/src/main/java/org/golarion/App.java
+++ app/src/main/java/org/golarion/App.java
@@ -3,15 +3,20 @@
  */
 package org.golarion;
 
-public class App
-{
-    public String getGreeting()
-    {
-        return "Hello World!";
-    }
+import javafx.application.Application;
+import javafx.stage.Stage;
 
+public class App extends Application
+{
     public static void main(String[] args)
     {
-        System.out.println(new App().getGreeting());
+        System.out.println("Hello, World!");
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception
+    {
+        stage.show();
     }
 }
