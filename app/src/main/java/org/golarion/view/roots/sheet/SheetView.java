5fb028ce187af98b6a023a428c0c1078c29e942a
ForzaElettromotrice <mingardi.federico@gmail.com>
2025-09-09 13:04:00 +0200

    Ora la sheetview ha le tab!

--- app/src/main/java/org/golarion/view/roots/sheet/SheetView.java
+++ app/src/main/java/org/golarion/view/roots/sheet/SheetView.java
@@ -1,5 +1,7 @@
 package org.golarion.view.roots.sheet;
 
+import javafx.scene.control.Tab;
+import javafx.scene.control.TabPane;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Pane;
 import org.golarion.model.Sheet;
@@ -14,6 +16,9 @@ public class SheetView implements GRoot
     private final Sheet relatedSheet;
 
     private final Pane root;
+
+    private final TabPane tabPane;
+
     private final CharacteristicView cView;
     private final SpellListView sView;
 
@@ -24,10 +29,30 @@ public class SheetView implements GRoot
         root = new AnchorPane();
         root.setPrefSize(800, 600);
 
+        tabPane = new TabPane();
+        tabPane.setPrefSize(800, 600);
+
+
         cView = new CharacteristicView(relatedSheet.getStats());
         sView = new SpellListView(relatedSheet.getSpellList());
 
-        root.getChildren().addAll(cView.getRoot());
+
+        Pane principal = new Pane(cView.getRoot());
+        Pane additional = new Pane();
+        Pane spells = new Pane(sView.getRoot());
+        Pane configuration = new Pane();
+
+        String[] names = {"Principale", "Addizionale", "Incantesimi", "Configurazione"};
+        Pane[] panes = {principal, additional, spells, configuration};
+        for (int i = 0; i < names.length; i++)
+        {
+            Tab tab = new Tab(names[i], panes[i]);
+            tab.setClosable(false);
+            tabPane.getTabs().add(tab);
+        }
+
+
+        root.getChildren().addAll(tabPane);
     }
 
 
